6e06ded4ea1f3cfe5d06b01a3c14773e14ec7107
sid <sid@sids-MacBook-Air.local>
2026-05-31 18:34:32 +0530

    Add siddDjango project

--- introDjango/siddDjango/settings.py
+++ introDjango/siddDjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chai'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'siddDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'stastic')]
--- introDjango/siddDjango/views.py
+++ introDjango/siddDjango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("hello world , you are in home page")
+    # return HttpResponse("hello world , you are in home page")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("hello world , you are in about page")
